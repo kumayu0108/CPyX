93bb277784ff4e9786dba5a824e99dc813d9e2e2
Ayush Kumar <kumayu@iitk.ac.in>
2022-01-31 23:18:20 +0530

    additional test cases added

---
+++ tests/Milestone_1/test6.c
@@ -0,0 +1,42 @@
+/* testing if,*/
+
+int main()
+{
+        if(1);
+
+        if(1){{1;}}
+
+        if(true)
+        {
+            printf("p1");
+        }
+
+        int x=6;
+
+        if(x==6)
+        {
+            printf("p2");
+        }
+
+        if((x-6))
+        {
+            prinf("p3");
+        }
+        else
+        {
+            prinf("p4");
+        }
+
+        if(x>0)
+        {
+            if(x>0)
+            {
+                if(x>0);
+            }
+        }
+
+        
+
+
+
+}
---
+++ tests/Milestone_1/test7.c
@@ -0,0 +1,33 @@
+/*
+Sanity check : Contains all reserved keywords in reverse order.
+*/
+
+int main()
+{
+    false;
+    true;
+    free;
+    calloc;
+    malloc;
+    scanf;
+    printf;
+    sizeof;
+    void;
+    return;
+    else;
+    if;
+    while;
+    for;
+    const;
+    struct;
+    bool;
+    float;
+    char;
+    int;
+    auto;
+
+    + - * / = == === += >>= >=;
+    
+
+    return 0;
+}
