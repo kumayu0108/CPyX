e34ec06ccd25cd0a5810fc4650c10b474bf6f0ff
Ayush Kumar <kumayu@iitk.ac.in>
2022-02-11 20:00:54 +0530

    Minor changes

--- tests/Milestone_1/test1.c
+++ tests/Milestone_1/test1.c
@@ -1,21 +1,4 @@
-/*
-Test commenting
-*/
 int main()
 {
-    // comment1
-    // comment2
-    /*comment3*/printf("test1\n");
-    /*
-    ////;;;
-    ///
-
-
-    comment4;;;
-    printf("inside comment\n");
-    ***
-    ***
-    */ 
-    return  /* comment5
-    */ 0;
+    int a = 5;
 }
