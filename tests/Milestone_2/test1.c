f82a49b60fd2ad8cef0c76fcf9e2331ae7801160
Subhrojyoti <66101999+subhroc173@users.noreply.github.com>
2022-02-18 17:46:49 +0530

    Added test1.c

---
+++ tests/Milestone_2/test1.c
@@ -0,0 +1,24 @@
+struct abc{
+    int x, y;
+};
+
+int main()
+{
+    int a = 1,b=5;
+    for(int i = 0; i < 10; i++){
+        printf("hey!");
+    }
+
+    while(i < 109){
+        int cx; 
+        for(int o; ; ){
+            
+        }
+    }
+
+    if(a == 1) printf("abcd\n");
+    if(b == 2) printf("pqrs\n");
+    else printf("wxyz\n");
+    
+    return 0;
+}
