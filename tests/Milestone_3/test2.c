0c4ca81d29809bbcfcdd06b0d2873180a236f516
Arpit Singh <arpitsg15@gmail.com>
2022-03-28 23:29:54 +0530

    Updated test2.c

--- tests/Milestone_3/test2.c
+++ tests/Milestone_3/test2.c
@@ -1,1 +1,26 @@
+int gcd(int a, int b)
+{
+    //Everything divides 0
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
 
+    // base case
+    if (a == b)
+        return a;
+
+    // a is greater
+    if (a > b)
+        return gcd(a - b, b);
+    return gcd(a, b - a);
+}
+
+// Driver program to test above function
+int main()
+{
+    // int a = 98, b = 56;
+    //printf("GCD of %d and %d is %d ", a, b, gcd(a, b));
+    int x = gcd(3,4);
+    return 0;
+}
